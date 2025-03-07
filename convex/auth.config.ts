0db8a1e25c73297a2631aab4d81dfc8d556c8cbe
sriram N V <sriramnvilvanathan@gmail.com>
2025-03-07 12:52:21 +0530

    added Convex client provider ts file to initialize convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://devoted-phoenix-76.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://devoted-phoenix-76.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
